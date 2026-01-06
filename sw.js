ae4b5268ce3ab6403a605f2d3e95b52f04da757b
ginesperezgalera-lang <ginesperezgalera@gmail.com>
2026-01-06 17:50:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,17 @@
-const CACHE_NAME = 'meditaciones-7-llamas-v1';
+const CACHE_NAME = 'decretos-yo-soy-v1';
 const urlsToCache = [
     './',
     './index.html',
     './manifest.json'
 ];
 
-// Instalar
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
     );
 });
 
-// Activar
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
-});
-
-// Fetch
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
@@ -37,16 +19,21 @@ self.addEventListener('fetch', event => {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request)
-                    .then(response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then(cache => cache.put(event.request, responseToCache));
-                        return response;
-                    });
+                return fetch(event.request);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName !== CACHE_NAME;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
     );
 });
